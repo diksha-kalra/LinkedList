d42fa9609beace32793c371e48ecc25d42ea6a54
diksha-kalra <kalradiksha11@gmail.com>
2020-10-11 18:17:16 +0530

    Added: peak and pop from stack until its empty

--- MyStackTest.java
+++ MyStackTest.java
@@ -4,32 +4,40 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyStackTest {
-	
+
 	@Test
 	public void given3NumbersWhenAddedToStackShouldHaveLastAddedNode() {
-		MyStack<Integer> myStack=new MyStack<>();
+		MyStack<Integer> myStack = new MyStack<>();
 		MyNode<Integer> myFirstNode = new MyNode<>(70);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode = new MyNode<>(56);
 		myStack.push(myFirstNode);
 		myStack.push(mySecondNode);
 		myStack.push(myThirdNode);
-		INode<Integer> myNode=myStack.peak();
+		INode<Integer> myNode = myStack.peak();
 		myStack.printStack();
-		Assert.assertEquals(myThirdNode,myNode);
+		Assert.assertEquals(myThirdNode, myNode);
 	}
-	
+
 	@Test
-	public void given3NumbersWhenPoppedShouldMatchWithLastAddedNode() {
-		MyStack<Integer> myStack=new MyStack<>();
+	public void given3NumbersPoppedFromStackTillListIsEmpty() {
+		MyStack<Integer> myStack = new MyStack<>();
 		MyNode<Integer> myFirstNode = new MyNode<>(70);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode = new MyNode<>(56);
 		myStack.push(myFirstNode);
 		myStack.push(mySecondNode);
 		myStack.push(myThirdNode);
-		INode<Integer> popedNode=myStack.pop();
+		INode<Integer> popedNode1 = myStack.pop();
 		myStack.printStack();
-		Assert.assertEquals(myThirdNode,popedNode);
+		Assert.assertEquals(myThirdNode, popedNode1);
+		INode<Integer> myNode = myStack.peak();
+		myStack.printStack();
+		Assert.assertEquals(mySecondNode, myNode);
+		INode<Integer> popedNode2 = myStack.pop();
+		myStack.printStack();
+		Assert.assertEquals(mySecondNode, popedNode2);
+		INode<Integer> popedNode3 = myStack.pop();
+		Assert.assertEquals(myFirstNode, popedNode3);
 	}
 }

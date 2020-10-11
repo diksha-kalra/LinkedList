f03d7d91144b71e6688a4cd25fe262d9ba75969d
diksha-kalra <kalradiksha11@gmail.com>
2020-10-11 18:31:52 +0530

    Added: creating queue using append method of linkedlist

---
+++ MyQueueTest.java
@@ -0,0 +1,20 @@
+package com.cg.linkedlist;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyQueueTest {
+	@Test
+	public void given3NumbersWhenAddedToQueueWhenPeakedShouldPointAtFirstElement() {
+		MyQueue<Integer> myQueue = new MyQueue<>();
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		myQueue.enqueu(myFirstNode);
+		myQueue.enqueu(mySecondNode);
+		myQueue.enqueu(myThirdNode);
+		INode<Integer> myNode = myQueue.peak();
+		myQueue.printQueue();
+		Assert.assertEquals(myFirstNode, myNode);
+	}
+}

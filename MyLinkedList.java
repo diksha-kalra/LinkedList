6043248347baa2b2d3c6a69cea0b6a3de8278910
diksha-kalra <kalradiksha11@gmail.com>
2020-10-11 13:55:30 +0530

    Added: insert node after particular node

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -68,7 +68,7 @@ public class MyLinkedList {
 		tempNode = tempNode.getNext();
 		return tempNode;
 	}
-	
+
 	public INode search(Integer key) {
 		INode tempNode = this.head;
 		INode found = null;
@@ -79,5 +79,16 @@ public class MyLinkedList {
 			tempNode = tempNode.getNext();
 		}
 		return found;
+	}
+
+	public void removeParticularNode(INode deleteNode) {
+		INode tempNode = this.head;
+		INode prev = null;
+		while (tempNode != null && tempNode.getKey() != deleteNode.getKey()) {
+			prev = tempNode;
+			tempNode = tempNode.getNext();
+		}
+		prev.setNext(tempNode.getNext());
+		tempNode.setNext(null);
 	}
 }

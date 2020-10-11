4776da90094aabe74386ec714d439078e7df0e0a
diksha-kalra <kalradiksha11@gmail.com>
2020-10-11 13:37:20 +0530

    Added: deleting last node from linked list

--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -65,4 +65,20 @@ public class MyLinkedListTest {
 				&& myLinkedList.tail.equals(myThirdNode);
 		Assert.assertTrue(result);
 	}
+	
+	@Test
+	public void givenThirdElementWhenDeletedShouldPassTest() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.append(mySecondNode);
+		myLinkedList.append(myThirdNode);
+		INode deletedNode = myLinkedList.popLast();
+		myLinkedList.printMyNode();
+		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
+				&& myLinkedList.tail.equals(mySecondNode);
+		Assert.assertTrue(result);
+	}
 }

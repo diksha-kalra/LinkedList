8e902c5f3d781e084312256af54812355736f435
diksha-kalra <kalradiksha11@gmail.com>
2020-10-11 18:15:09 +0530

    Added: peak and pop from  stack till empty

--- MyStack.java
+++ MyStack.java
@@ -1,23 +1,25 @@
 package com.cg.linkedlist;
 
-public class MyStack<K extends Comparable<K>>{
+public class MyStack<K extends Comparable<K>> {
 	private final MyLinkedList<K> myLinkedList;
-	
+
 	public MyStack() {
-		myLinkedList= new MyLinkedList<>();
+		myLinkedList = new MyLinkedList<>();
 	}
-	
+
 	public void push(INode<K> element) {
 		myLinkedList.add(element);
 	}
-	
-	public INode<K> peak(){
+
+	public INode<K> peak() {
 		return myLinkedList.head;
 	}
+
 	public void printStack() {
 		myLinkedList.printMyNode();
 	}
-	
 
-
+	public INode<K> pop() {
+		return myLinkedList.pop();
+	}
 }
--- MyStackTest.java
+++ MyStackTest.java
@@ -18,4 +18,18 @@ public class MyStackTest {
 		myStack.printStack();
 		Assert.assertEquals(myThirdNode,myNode);
 	}
+	
+	@Test
+	public void given3NumbersWhenPoppedShouldMatchWithLastAddedNode() {
+		MyStack<Integer> myStack=new MyStack<>();
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		myStack.push(myFirstNode);
+		myStack.push(mySecondNode);
+		myStack.push(myThirdNode);
+		INode<Integer> popedNode=myStack.pop();
+		myStack.printStack();
+		Assert.assertEquals(myThirdNode,popedNode);
+	}
 }
